f90706d0b8269dc504999f5882729650b62ba094
Alex Chan <alex.chan@berkeley.edu>
2017-07-31 18:54:19 -0700

    Save links in options page

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ The Google Chrome extension for keeping you focused.
 Track your usage of distracting websites (i.e. Facebook, Youtube, Reddit).
 
 Start limiting either the time or number of visits for these pages.
+
+Icon designed by Rosa Choe
--- options.js
+++ options.js
@@ -1,6 +1,8 @@
 //upon list add, begin tracking
 
 document.addEventListener("DOMContentLoaded", function(){
+  restore_options();
+  document.getElementById('save').addEventListener('click', save_options);
   addInput = document.getElementById("addSite");
   addInput.addEventListener("click", function() {
     var input = document.getElementById("url");
@@ -9,8 +11,41 @@ document.addEventListener("DOMContentLoaded", function(){
       var li = document.createElement("LI");
       li.innerHTML = url;
       input.value = "";
-
       document.getElementById("urlList").appendChild(li);
     }
   });
 });
+
+function save_options() {
+  var ul = document.getElementById("urlList");
+  var items = ul.getElementsByTagName("li");
+  var links = []
+  for (var i = 0; i < items.length; i++) {
+    links.push(items[i].innerHTML);
+  }
+  var status = document.getElementById('status');
+  status.textContent = 'Options saved.';
+
+  chrome.storage.sync.set({
+    urls: links
+  }, function() {
+    var status = document.getElementById("status");
+    status.textContent = "Options Saved";
+    console.log(links);
+    setTimeout(function() {
+      status.textContent = "";}, 750);
+  });
+}
+
+function restore_options() {
+  chrome.storage.sync.get({
+    urls: []
+  }, function(items) {
+    for (var i = 0; i < items.urls.length; i++) {
+      var li = document.createElement("LI");
+      li.innerHTML = items.urls[i];
+      document.getElementById("urlList").appendChild(li);
+      console.log(items.urls[i]);
+    }
+  });
+}
